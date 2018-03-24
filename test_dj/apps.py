d7f67c2222e4ccd75ba1498245ca98e31167d301
seo <saltae99@gmail.com>
2018-03-24 12:00:38 +0900

    My Django Girls app, first commit

---
+++ test_dj/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TestDjConfig(AppConfig):
+    name = 'test_dj'
---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/poppo99/Desktop/djanggo_pr1/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
